20902e333a6961ab1a1f3756e28bf0641529d63c
swetha <swetha.arige@gmail.com>
2022-01-26 00:22:17 -0800

    Search in sorrted matrx

---
+++ AlgoExpertMedium/src/SearchInSortedMatrix.java
@@ -0,0 +1,22 @@
+public class SearchInSortedMatrix {
+    public static void main(String[] args) {
+        /*int[][] matrix = {{1,4,7}, {2, 3,9}, {5,8,9}};
+        int[] result = searchInSortedMatrix(matrix);*/
+        System.out.println("Result ");
+    }
+    //O(n+m) Time and O(1) Space
+    public static int[] searchInSortedMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] > target) {
+                col--;
+            } else if (matrix[row][col] < target) {
+                row++;
+            } else {
+                return new int[] {row, col};
+            }
+        }
+        return new int[] {-1, -1};
+    }
+}
