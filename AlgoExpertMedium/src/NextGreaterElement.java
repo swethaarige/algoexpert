4d6572ca578c4a413f866492626b78b70338f5bf
narigala <nagaswetha_arigala@bmc.com>
2022-01-26 20:53:19 -0800

    Next Greater Elements

--- AlgoExpertMedium/src/NextGreaterElement.java
+++ AlgoExpertMedium/src/NextGreaterElement.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Stack;
+
 /*
 Find next greater elements in an array
 1. Brute-Force Approach
@@ -17,5 +20,31 @@ Then push the current element x on top of the stack.
 Repeat the process for every array element.
 https://www.enjoyalgorithms.com/blog/find-next-greater-element
  */
+// O(n) Time and O(n) Space
 public class NextGreaterElement {
+    public static void main (String[] args) {
+        int[] inputArray = {2, 5, -3, -4, 6, 7, 2};
+        int[] outputArray = nextGreaterElement(inputArray);
+        for (int i = 0; i < outputArray.length; i++) {
+            System.out.println("Next Greater elements result array is " + outputArray[i]);
+        }
+
+    }
+    public static int[] nextGreaterElement(int[] array) {
+        int[] result = new int[array.length];
+        Arrays.fill(result, -1);
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < 2 * array.length; i++) {
+             int circularIdx = i % array.length;
+             while (stack.size() > 0 && array[stack.peek()] < array[circularIdx]) {
+                 int top = stack.pop();
+                 result[top] = array[circularIdx];
+             }
+             stack.push(circularIdx);
+        }
+        return result;
+    }
+
 }
