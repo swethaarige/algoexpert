ae47800612ba07d4c12697c15298724f7cacb5f5
swetha <swetha.arige@gmail.com>
2022-02-03 19:59:32 -0800

    InvertBinary Tree second approach

--- AlgoExpertMedium/src/InvertBinaryTree.java
+++ AlgoExpertMedium/src/InvertBinaryTree.java
@@ -1,6 +1,23 @@
+import java.util.ArrayDeque;
+
 /*
 Given the root of a binary tree, invert the tree,
 The function should swap every left node in the tree to its corresponding right node.
+1. Recursive Solution O(n) Time and O(d) Space
+This is one of the most famous interview questions and can be easily solved recursively.
+The idea is to traverse the tree in a preorder fashion, and for every node encountered,
+swap its left and right child before recursively inverting its left and right subtree.
+2. Iterative solution using queue
+Steps to invert a Binary Tree iteratively using Queue:
+If root/ current node is NULL, inverting is done.
+Define a queue Q.
+Add root node to queue Q.
+While queue Q is not empty:
+4.1. Pop node N from queue Q from left side.
+4.2. Swap left and right child of node N
+4.3. Push new left and right child of node N to queue Q.
+The Binary Tree is inverted.
+Iterative solution using queue	O(n)	O(n)
  */
 //O(n) Time and O(d) space
 public class InvertBinaryTree {
@@ -25,6 +42,20 @@ public class InvertBinaryTree {
 
         public BinaryTree(int value) {
             this.value = value;
+        }
+    }
+    public static void invertBinaryTree2(BinaryTree tree) {
+        ArrayDeque<BinaryTree> queue = new ArrayDeque<>();
+        queue.addLast(tree);
+        while (queue != null) {
+            BinaryTree current = queue.pollFirst();
+            swapLeftAndRight(current);
+            if (current.left != null) {
+                queue.addLast(current.left);
+            }
+            if (current.right != null) {
+                queue.addLast(current.right);
+            }
         }
     }
 }
