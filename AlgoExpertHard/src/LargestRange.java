a9e4af81623da5297d0d3ad04d0a9921240a8404
swetha <swetha.arige@gmail.com>
2022-02-24 12:48:02 -0800

    Largest Range

---
+++ AlgoExpertHard/src/LargestRange.java
@@ -0,0 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class LargestRange {
+    public static void main (String[] args) {
+        int[] array = {1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6};
+        int[] result = largestRange(array);
+        System.out.println("Largest Range is " + result[0] + "  " +result[1]);
+    }
+    public static int[] largestRange(int[] array) {
+        Map<Integer, Boolean> nums = new HashMap<>();
+        int longest = 0;
+        int[] longestRange = new int[2];
+        for (int num : array) {
+            nums.put(num, true);
+        }
+        for (int num : array) {
+            if (!nums.containsKey(num)) {
+                continue;
+            }
+            nums.put(num, false);
+            int currentLength = 1;
+            int left = num - 1;
+            int right = num + 1;
+
+            while (nums.containsKey(left)) {
+                nums.put(left, false);
+                currentLength += 1;
+                left = left - 1;
+
+            }
+            while (nums.containsKey(right)) {
+                nums.put(right, false);
+                currentLength += 1;
+                right = right + 1;
+            }
+            if (longest < currentLength) {
+                longest = currentLength;
+                longestRange = new int[] {left + 1, right - 1};
+            }
+        }
+        return longestRange;
+    }
+}
