90163d028d5ecc7c5c4237fdb568390648316651
narigala <nagaswetha_arigala@bmc.com>
2022-03-05 21:27:35 -0800

    TRies

--- AlgoExpertMedium/src/TrieSearchInsert.java
+++ AlgoExpertMedium/src/TrieSearchInsert.java
@@ -1,4 +1,21 @@
-import java.util.Scanner;
+/*
+
+Trie is an efficient information reTrieval data structure.
+Using Trie, search complexities can be brought to optimal limit (key length).
+If we store keys in binary search tree, a well balanced BST will need time proportional to M * log N,
+where M is maximum string length and N is number of keys in tree. Using Trie, we can search the key in O(M) time.
+However the penalty is on Trie storage requirements
+Tries is a tree that stores strings. The maximum number of children of a node is equal to the size of the alphabet.
+Trie supports search, insert and delete operations in O(L) time where L is the length of the key.
+Why Trie? :-
+
+With Trie, we can insert and find strings in O(L) time where L represent the length of a single word.
+This is obviously faster than BST. This is also faster than Hashing because of the ways it is implemented.
+We do not need to compute any hash function. No collision handling is required (like we do in open addressing and separate chaining)
+Another advantage of Trie is, we can easily print all words in alphabetical order which is not easily possible with hashing.
+We can efficiently do prefix search (or auto-complete) with Trie.
+ tries data structure is that they are faster but require huge memory for storing the strings.
+ */
 
 public class TrieSearchInsert {
     private static TrieNode root;
